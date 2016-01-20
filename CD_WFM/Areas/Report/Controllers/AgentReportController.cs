23da619d3946e08f0de207f7303dba9c7598097b
zairczhu <zaric.zhu@foxmail.com>
2016-01-20 18:39:28 +0900

    add some page

--- CD_WFM/Areas/Report/Controllers/AgentReportController.cs
+++ CD_WFM/Areas/Report/Controllers/AgentReportController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WFM.Controllers;
 
 namespace WFM.Areas.Report.Controllers
 {
-    public class AgentReportController : Controller
+    public class AgentReportController : BaseController
     {
         // GET: Report/Agent
         public ActionResult Index()
